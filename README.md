f8ccbacb2b9b53726ee2bf84682928565926fb45
mingxianwei <mignxianwie007@gmail.com>
2017-03-16 16:39:17 +0800

    初始化我的Swift工程

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MySwiftLearning
 我的第一个swift工程
+— 
++ 3月16日开始初始化我的Swift工程
++ 
---
+++ SwiftStart/SwiftStart/Hello.swift
@@ -0,0 +1,14 @@
+//
+//  Hello.swift
+//  SwiftStart
+//
+//  Created by mingxianwei on 2017/3/16.
+//  Copyright © 2017年 mingxianwei. All rights reserved.
+//
+
+import Cocoa
+
+class Hello: NSObject {
+
+    
+}
