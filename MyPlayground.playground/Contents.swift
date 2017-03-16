5416b18ab322ef1236f7df6f30d3738afbccab44
mingxianwei <mignxianwie007@gmail.com>
2017-03-16 18:05:38 +0800

    1、添加Payground 2、添加一个Swift教程

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -7,3 +7,40 @@ print("hello")
 
 var myVliable = 42
 myVliable = 50
+
+let myConstant = 77
+
+
+var myVliable2 : NSInteger = 70
+let myConstfloat : Float = 4
+
+let label = "the fucking word"
+let width = 94
+let widthLabel = label + " " + String(width)
+
+let apples = 3
+let oranges = 5
+
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = " I have \(apples + oranges) pieces of fruit."
+//use\() transform a float value to a string value and add a nickNames
+
+let flotValue = 3.5
+let myExercise = "\(flotValue)" + " mingxianwei"
+
+
+var shoppingList = ["catfish","water","tulips","blue paint"]
+shoppingList[1]
+var occupations = [
+    "Malcolm":"captain",
+    "Kaylee" : "Mechainc"
+]
+occupations["Jayne"] = "Publick Relations"
+
+print(occupations)
+
+var emptyArray = [String]()
+
+
+
+
