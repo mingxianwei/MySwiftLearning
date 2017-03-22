604bbcf016f4a37db438f6ca1426827bc524a645
mingxianwei <mignxianwie007@gmail.com>
2017-03-22 13:40:44 +0800

    1、初步认识函数和闭包 完成对基本数据类型的认识和了解以及基本的处理方式方法。

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -40,6 +40,78 @@ occupations["Jayne"] = "Publick Relations"
 print(occupations)
 
 var emptyArray = [String]()
+let emptDictonary = [String : Float]()
+
+// control statements
+let individualSource:[Int] = [75,43,103,87,12]
+let individusalSource:[String:Int] = [
+    "China":123,
+    "America": 11
+]
+var teasmSource = 0
+var maxSource = individualSource[0]
+
+for source in individualSource {
+    if source > 50 {
+        teasmSource += source
+    } else {
+        teasmSource -= source
+    }
+    if source > maxSource {
+        maxSource = source
+    }
+}
+
+print("\(teasmSource)")
+print("\(maxSource)")
+
+var optionalString:String? = nil
+print(optionalString == nil)
+
+var greeting:String = "hello"
+
+if let name = optionalString {
+    greeting = "hello \(name)"
+    print(greeting)
+}
+
+let nickName: String? = "mingxianwei"
+let fullName: String = "Andrew Ming"
+
+let informalGreeting = "hi \(nickName ?? fullName)"
+
+
+
+var mutipleN: Int = 2
+
+while mutipleN < 100 {
+    mutipleN *= 2
+}
+
+print("\(mutipleN)")
+var mutipuleM = 2
+repeat {
+    mutipuleM *= 2
+} while  mutipuleM < 100
+print(mutipuleM)
+
+var total = 0
+for var i in 0..<4 {
+    total += i
+}
+
+print(total)
+
+
+/** 使用 function 来声明一个函数 使用-> 来指定函数返回值得类型*/
+func echo(person:String,day:String) -> String {
+    return "Hello \(person),today is \(day)."
+}
+
+var myHello: String = echo(person: "MING", day: "2017-07-89")
+
+print(myHello)
+
 
 
 
