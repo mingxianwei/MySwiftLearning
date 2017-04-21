23d00e908b973f915703624c924e43dc06472653
mingxianwei <mignxianwie007@gmail.com>
2017-04-21 18:05:35 +0800

    新建文件及文本

---
+++ SwiftStart/SwiftStart/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftStart
+//
+//  Created by mingxianwei on 2017/4/21.
+//  Copyright © 2017年 mingxianwei. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
